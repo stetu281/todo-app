e448fe3ef560e2757c66e8d2b57b23d37be1fe57
Stefan Turner <turner.stefan281@gmail.com>
2021-03-21 10:20:05 +0100

    Change header-icon to button

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-document.querySelector('.header__icon').addEventListener('click', (e) => {
+
+
+document.querySelector('.header__button').addEventListener('click', (e) => {
     document.body.classList.toggle('light-mode');
-    e.target.src = 'images/icon-moon.svg';
+    if(document.body.classList.contains('light-mode')) {
+        e.target.src = "images/icon-moon.svg";
+    } else {
+        e.target.src = "images/icon-sun.svg";
+    }
 })
