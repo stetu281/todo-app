a904516a38b7937a788741ce4a69b7a4c083c620
Stefan Turner <turner.stefan281@gmail.com>
2021-04-11 20:37:24 +0200

    Clean start

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,45 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: {
+      'josefin': 'Josefin Sans, sans-serif',
+    },
+
+    colors: {
+      primary: {
+        'brightBlue': 'hsl(220, 98%, 61%)',
+        'gradiant1': 'hsl(192, 100%, 67%)',
+        'gradiant2': 'hsl(280, 87%, 65%)',
+      },
+      lightTheme: {
+        blue: {
+          'vLight': 'hsl(236, 33%, 92%)',
+          'light': 'hsl(233, 11%, 84%)',
+          'dark': 'hsl(236, 9%, 61%)',
+          'vDark': 'hsl(235, 19%, 35%)',
+        },
+        gray: {
+          'vLight': 'hsl(0, 0%, 98%)',
+        },
+      },
+      darkTheme: {
+        blue: {
+          'vDark': 'hsl(235, 21%, 11%)',
+          'vDarkDesat': 'hsl(235, 24%, 19%)',
+          'light': 'hsl(234, 39%, 85%)',
+          'lightHover': 'hsl(236, 33%, 92%)',
+          'dark': 'hsl(234, 11%, 52%)',
+          'vDark1': 'hsl(233, 14%, 35%)',
+          'vDark2': 'hsl(237, 14%, 26%)',
+        },
+      },
+    },
+
+    extend: {
+      screens: {
+        'desktop': '1440px',
+      },
+    },
   },
   variants: {
     extend: {},
