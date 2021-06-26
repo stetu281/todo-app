ec56eeee2b6a17dd217021abcd2667fb72dad21a
Stefan Turner <turner.stefan281@gmail.com>
2021-06-26 11:50:14 +0200

    Add tasks to list with button and enter

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+import { removeElement, delegate } from './tools.js';
+
 //light und darkmode umschalten
 document.querySelector('.header__button').addEventListener('click', (e) => {
     document.body.classList.toggle('light-mode');
@@ -8,6 +10,7 @@ document.querySelector('.header__button').addEventListener('click', (e) => {
     }
 })
 
+//tasks
 const tasks = [
     {task: "Complete online Javascript course", checked: true},
     {task: "Wash dishes", checked: false},
@@ -16,18 +19,47 @@ const tasks = [
     {task: "Go shopping", checked: true}
 ]
 const list = document.querySelector('.list-items');
+const taskInput = document.querySelector('.create-item__input');
 
-for(let [index, task] of tasks.entries()) {
-    const li = document.createElement('li');
-    li.id = index;
-    li.classList = 'list-items__item';
-    li.innerHTML = `
-        <label class="list-items__checkbox-label">
-            <input type="checkbox" class="list-items__checkbox">
-            <div class="list-items__status list-items__status--checked"></div>
-            <span class="list-items__description list-items__description--done">${task.task}</span>
-        </label>
-        <img class="list-items__close" src="images/icon-cross.svg" alt="">
-    `;
-    list.appendChild(li);
-}
+render(tasks);
+
+
+function render(tasks) {
+    list.innerHTML = '';
+
+    for(let [index, task] of tasks.entries()) {
+        const li = document.createElement('li');
+        li.id = index;
+        li.classList = 'list-items__item';
+        li.innerHTML = `
+            <label class="list-items__checkbox-label">
+                <input type="checkbox" class="list-items__checkbox" ${task.checked ? 'checked' : ''}>
+                <div class="list-items__status list-items__status--checked"></div>
+                <span class="list-items__description list-items__description--done">${task.task}</span>
+            </label>
+            <img class="list-items__close" src="images/icon-cross.svg" alt="">
+        `;
+        list.appendChild(li);
+    }
+};
+
+function addItem() {
+    if(taskInput.value === '') {
+        alert('Leer');
+    } else {
+        const input = {task: taskInput.value, checked: false};
+        taskInput.value = '';
+        tasks.push(input);
+        render(tasks);
+    };
+};
+
+
+//Task zur Liste hinzufügen
+document.querySelector('.create-item__button').addEventListener('click', addItem)
+taskInput.addEventListener('keyup', (e) => {
+    if(e.keyCode === 13) {
+        addItem();
+    };
+});
+
