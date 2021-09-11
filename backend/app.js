7c1233b3bf27a594a4cb94afc972468205048951
Stefan Turner <turner.stefan281@gmail.com>
2021-09-11 06:29:15 +0200

    Get todos and post todo requests  working

---
+++ backend/app.js
@@ -0,0 +1,65 @@
+import express from 'express';
+import mysql from 'mysql2/promise';
+
+const app = express();
+app.use(express.json());
+const port = 3000;
+
+const connection = await mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "todo"
+});
+
+app.get('/todos', async (req, res) => {
+    const [todos] = await connection.query(`
+        SELECT id, title, completed FROM task
+    `);
+    res.status(200);
+    res.send(todos);
+});
+
+app.post('/todos', async (req, res) => {
+    if(!req.body) {
+        res.status(400);
+        res.send({error: "body is missing"});
+        return;
+    }
+    if(!req.body.title) {
+        res.status(400);
+        res.send({error: "title is missing"});
+        return;
+    }
+    if(typeof req.body.completed == "undefined" || typeof req.body.completed != "number") {
+        res.status(400);
+        res.send({error: "completed is missing or not a number"});
+        return;
+    }
+
+    const [result] = await connection.execute(`
+        INSERT INTO task (title, completed) VALUES (?, ?)
+    `, [req.body.title, req.body.completed]);
+
+    const [todos] = await connection.query(`
+        SELECT id, title, completed FROM task WHERE id = ?
+    `, [result.insertId]);
+
+    res.status(201);
+    res.send(todos);
+})
+
+
+const server = app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
+const shutdownHandler = () => {
+    console.log('closing all connections...');
+    server.close(() => {
+        connection.destroy();
+        console.log('shutting down...');
+        process.exit();
+    });
+};
+process.on('SIGINT', shutdownHandler);
+process.on('SIGTERM', shutdownHandler);
